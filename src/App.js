f40b12171ded7d9e568209f5d42f608a739e1ab5
krushna rathod <krushna.rathod@simformsolutions.com>
2025-04-25 10:37:37 +0530

    SCAFFOLD: add name, email and github account in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <p>Hello, my name is Krushna Rathod.</p>
+      <p>
+        My email is:
+        <a href="mailto:krushna.rathod@simformsolutions.com">
+          krushna.rathod@simformsolutions.com
+        </a>
+      </p>
+      <p>
+        My GitHub account:
+        <a href="https://github.com/krushna-simform">
+          github.com/krushna-simform
+        </a>
+      </p>
     </div>
   );
 }
